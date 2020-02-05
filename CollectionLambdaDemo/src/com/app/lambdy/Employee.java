326b969b1e3a385f2abb733f5a32755eaddacaf2
Suryakanth Jwala <Suryakanth Jwala@DESKTOP-0LURAB4>
2020-02-05 18:00:26 +0530

    Employee Class

---
+++ CollectionLambdaDemo/src/com/app/lambdy/Employee.java
@@ -0,0 +1,14 @@
+package com.app.lambdy;
+
+public class Employee {
+
+	public static void main(String[] args) {
+
+		String name="Surya";
+		System.out.println("My Name is:"+name);
+		
+		
+		
+	}
+
+}
